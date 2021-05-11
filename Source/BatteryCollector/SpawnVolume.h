d73d2841a562833c513d0b54eb8ca33e79924bed
Yongmin Cho <qlxhdl55@gmail.com>
2021-05-11 12:29:45 -0700

    Physics of Battery

---
+++ Source/BatteryCollector/SpawnVolume.h
@@ -0,0 +1,36 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "SpawnVolume.generated.h"
+
+UCLASS()
+class BATTERYCOLLECTOR_API ASpawnVolume : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	ASpawnVolume();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+    //Returns the WhereToSpawn subObject
+    FORCEINLINE class UBoxComponent* GetWhereToSpawn() const { return WhereToSpawn; }
+
+    //Find a random point within the Boxcomponent
+    UFUNCTION(BlueprintPure, Category = "Spawning")
+    FVector GetRandomPointInVolume();
+private:
+    //Box Component to specify where pickups should be spawned
+    UPROPERTY(VisibleAnyWhere, BlueprintReadOnly, Category = "Spawning", meta = (AllowPrivateAccess = "true"))
+    class UBoxComponent* WhereToSpawn;    
+};
